b0240687bd65c04c5e663c28bf1629cc2bc2bf1c
notiom <99580052+notiom@users.noreply.github.com>
2024-03-27 15:42:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir ../datasets --save_dir ../results --model_id 19
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir ../datasets --save_dir ../results --model_id 19 --include_test
